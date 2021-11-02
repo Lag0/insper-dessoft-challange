7f90be4b1587fc259f77e4a2b0ac2c4692b558ce
Luca Mizrahi <lucam1@al.insper.edu.br>
2021-11-02 20:14:23 -0300

    Criando as peças do dominó

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 # COMECANDO O CODIGO
+
+# Criando as peças do jogo
+
+import random
+
+def cria_pecas():
+    pecas = []
+
+    i = 0
+    while i <= 6:
+
+        a = 0
+        while a <= 6:
+            if [i, a] not in pecas and [a, i] not in pecas:
+                pecas.append[i, a]
+            a += 1
+            
+        i += 1
+    
+    random.shuffle(pecas)
+    return pecas
+
+
