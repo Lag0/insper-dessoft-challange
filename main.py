644c6b9fdd0c7ea8b92992a2c8804aeb0b3d40d2
brunolag0 <brunorl3@al.insper.com.br>
2021-11-03 18:20:50 -0300

    Adicionar na Mesa e algumas formatações

--- main.py
+++ main.py
@@ -52,23 +52,43 @@ def posicoes_possiveis(mesa, pecas):
             posicoes.append(c)
     return posicoes
 
+def adiciona_na_mesa(peca, mesa):
+    if len(mesa) == 0:
+        mesa.append(peca)
+        return mesa
+
+    p1 = peca[0] # primeiro número da peça
+    p2 = peca[1] # segundo número da peca
+
+    mesa1 = mesa[0][0] # primeiro número da primeira peça
+    mesa2 = mesa[-1][1] # Último número da última peça
+
+    if p1 == mesa1:
+        inv = [p2, p1]
+        mesa.insert(0, inv) # Peça invertida na mesa
+
+    elif p2 == mesa1:
+        mesa.insert(0, peca) # Peça na posição original na mesa
+
+    elif p1 == mesa2:
+        mesa.append(peca) # Peca no final da mesa
+
+    elif p2 == mesa2:
+        inv = [p2, p1]
+        mesa.append(inv) # Peça invertida na mesa
+        
+    return mesa
 
 #---------------------------------------------------------------#
-
-# IGNORE - TEST ONLY
+#                       IGNORE - TEST ONLY                      #
 jogadores = 4
 peças = cria_pecas()
 dicionario = (inicia_jogo(jogadores, peças))
-print(verifica_ganhador(
-                        {
-                        0: [[1, 3], [0, 1], [4, 6], [0, 3], [0, 4], [6, 6], [0, 6]], 
-                        1: [], 
-                        2: [[3, 6], [2, 4], [2, 5], [2, 6], [3, 3], [3, 4], [2, 3]], 
-                        3: [[3, 5], [4, 4], [4, 5], [0, 2], [5, 5], [5, 6], [0, 5]]
-                        }))
-
-
-#----------------------------------------------------------------#
+pecas_testes = dicionario['jogadores'][0]
+pecas_testes2 = dicionario['jogadores'][0][1]
+print(adiciona_na_mesa(pecas_testes2, mesa))
+#                       SOMENTE PARA TESTE                      #
+#---------------------------------------------------------------#
 
 
 
