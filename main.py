52e8113818ce1b9f6ddae52df3a2d4383a7444bf
brunolag0 <brunorl3@al.insper.com.br>
2021-11-03 12:41:05 -0300

    Função Inicia Jogo

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 # COMECANDO O CODIGO
 
+# Importações Necessárias
+import random
+
 # Criando as peças do jogo
-
-import random 
-
 def cria_pecas():
-    pecas = []
+    peças = []
     for i in range(7):
-
         for a in range(7):
-            if [i, a] not in pecas and [a, i] not in pecas:
-                pecas.append([i, a])
-    random.shuffle(pecas)
+            if [i, a] not in peças and [a, i] not in peças:
+                peças.append([i, a])
+    random.shuffle(peças)
 
-    return pecas
+    return peças
 
+def inicia_jogo(jogadores, peças):
+    lista = {'jogadores': {}, 'monte': [], 'mesa': []}
+    for i in range(jogadores):
+        lista['jogadores'][i] = peças[0:7]
+        del peças[0:7]
+    lista['monte'] = peças
+    return lista
+
+jogadores = 2
+peças = cria_pecas()
+print(f'Lista Peças: {peças}...')
+print(inicia_jogo(jogadores, peças))
 
