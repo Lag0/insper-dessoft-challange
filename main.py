8984fc06d8e6e0ba9e455b237887409948d120bd
brunolag0 <brunorl3@al.insper.com.br>
2021-11-03 13:02:24 -0300

    Função Verifica Ganhador

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# COMECANDO O CODIGO
-
 # Importações Necessárias
 import random
 
@@ -11,19 +9,33 @@ def cria_pecas():
             if [i, a] not in peças and [a, i] not in peças:
                 peças.append([i, a])
     random.shuffle(peças)
-
     return peças
 
+# Iniciando o Jogo
 def inicia_jogo(jogadores, peças):
-    lista = {'jogadores': {}, 'monte': [], 'mesa': []}
+    dicionario = {'jogadores': {}, 'monte': [], 'mesa': []}
     for i in range(jogadores):
-        lista['jogadores'][i] = peças[0:7]
+        dicionario['jogadores'][i] = peças[0:7]
         del peças[0:7]
-    lista['monte'] = peças
-    return lista
+    dicionario['monte'] = peças
+    return dicionario
 
-jogadores = 2
+# Verificando Ganhador
+def verifica_ganhador(dicionario):
+    for k in dicionario.keys():
+        if dicionario[k] == []:
+            return k
+    else: return -1
+
+# IGNORE - TEST ONLY
+jogadores = 4
 peças = cria_pecas()
-print(f'Lista Peças: {peças}...')
-print(inicia_jogo(jogadores, peças))
+dicionario = (inicia_jogo(jogadores, peças))
+print(verifica_ganhador(
+                        {
+                        0: [[1, 3], [0, 1], [4, 6], [0, 3], [0, 4], [6, 6], [0, 6]], 
+                        1: [], 
+                        2: [[3, 6], [2, 4], [2, 5], [2, 6], [3, 3], [3, 4], [2, 3]], 
+                        3: [[3, 5], [4, 4], [4, 5], [0, 2], [5, 5], [5, 6], [0, 5]]
+                        }))
 
