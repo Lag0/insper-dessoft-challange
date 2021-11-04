e1e6a2264fcd9ca56c2d3158c0db58be5f8e70ee
brunolag0 <brunorl3@al.insper.com.br>
2021-11-04 18:50:49 -0300

    PPRINT

--- Dominó.py
+++ Dominó.py
@@ -1,20 +1,20 @@
 # Importando funções e bibliotecas
 
-from random import*
-from main import*
+import pprint
+from random import *
+from main import *
 
 # Começando o jogo
 flag = True
 while flag:
     n_jogadores = int(input('Qual o número o de jogadores: '))
     if n_jogadores > 4 or n_jogadores < 2:
-        print('Número inválido, escolher um número entre 2 e 4')
-    elif n_jogadores <= 4 and n_jogadores >= 2:
+        print('Número inválido, favor escolher um número entre 2 e 4!')
+    else:
         flag = False
 
 pecas = cria_pecas()
+inicia = inicia_jogo(n_jogadores, pecas)
 
-
-a = inicia_jogo(n_jogadores, pecas)
-print(a)
+pprint.pprint(inicia)
 
