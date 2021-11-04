7609c070ff90eb77d8587a7d23a82fd4fffcbd3c
brunolag0 <brunorl3@al.insper.com.br>
2021-11-04 19:27:36 -0300

    jogo v1

--- Dominó.py
+++ Dominó.py
@@ -6,15 +6,25 @@ from main import *
 
 # Começando o jogo
 flag = True
+continuar = True
+
 while flag:
-    n_jogadores = int(input('Qual o número o de jogadores: '))
+    n_jogadores = int(input('Qual o número o de jogadores [2-4]: '))
     if n_jogadores > 4 or n_jogadores < 2:
         print('Número inválido, favor escolher um número entre 2 e 4!')
     else:
         flag = False
-
+        
 pecas = cria_pecas()
 inicia = inicia_jogo(n_jogadores, pecas)
+mesa = inicia['mesa']
+jogadores_pecas = inicia['jogadores']
+monte = inicia['monte']
 
-pprint.pprint(inicia)
+print(f'No jogo temos: \n{n_jogadores} jogadores com 7 peças cada!\n')
+print(f'Tamanho do monte: {len(monte)} peças')
+print(f"Mesa Atual: {mesa}")
 
+
+#print(f"Jogador {}")
+#print(f"Colocou {}")
