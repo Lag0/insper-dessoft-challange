4097d329d564b46f8ffc5cf354495f250bb163ad
brunolag0 <brunorl3@al.insper.com.br>
2021-11-04 18:41:22 -0300

    Retirando Modulos Externos

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 # Importações Necessárias
 import random
-import pprint
-from yachalk import chalk
 
 # Criando as peças do jogo
 def cria_pecas():
@@ -81,15 +79,6 @@ def adiciona_na_mesa(peca, mesa):
         
     return mesa
 
-# Formatando INICIA_JOGO
-def formatado_inicia_jogo(dicionario):
-    print(chalk.red.italic("Peças dos Jogadores:"))
-    pprint.pprint(dicionario['jogadores'])
-    print(chalk.red.italic("Monte Atual:"))
-    pprint.pprint(dicionario['monte'])
-    print(chalk.red.italic("Mesa Atual:"))
-    pprint.pprint(dicionario['mesa'])
-
 #---------------------------------------------------------------#
 #                       IGNORE - TEST ONLY                      #
 jogadores = 3
@@ -97,7 +86,6 @@ peças = cria_pecas()
 dicionario = (inicia_jogo(jogadores, peças))
 pecas_testes = dicionario['jogadores'][0]
 pecas_testes2 = dicionario['jogadores'][0][1]
-formatado_inicia_jogo(dicionario)
 #                       SOMENTE PARA TESTE                      #
 #---------------------------------------------------------------#
 
