0e4cd6e123b3283c469eeabc12cf341efafa87cf
brunolag0 <brunorl3@al.insper.com.br>
2021-11-04 22:10:09 -0300

    bugfixes

--- Dominó.py
+++ Dominó.py
@@ -34,26 +34,35 @@ print(f'No jogo temos: \n{n_jogadores} jogadores com 7 peças cada!\n')
 # Função de formatação do jogo
 while len(jogadores_pecas[0]) > 0:
     def formata():
-        print(chalk.blue_bright(f'Tamanho do monte: {len(monte)} peças'))
-        print(chalk.blue(f"Mesa Atual: {mesa}\n"))
+        
+        print(chalk.blue(f'Tamanho do monte: {len(monte)} peças'))
+        print(chalk.yellow(f"Mesa Atual: {mesa}\n"))
+        
+        print("-"*30)
+        
         print(chalk.green('Jogador 0:'))
-        print(chalk.magenta_bright(jogadores_pecas[0]))
-        print("="*55)
-        print("Posições Possiveis: ")
+        print(chalk.magenta(jogadores_pecas[0]))
+        
+        print("-"*30)
+        
+        print("\nPosições Possiveis: ")
         print(posicoes_possiveis(mesa, jogadores_pecas[0]))
+        
         jogar = int(input('Qual peça você quer jogar: '))
         adiciona_na_mesa(jogadores_pecas[0][jogar], mesa)
         jogadores_pecas[0].pop(jogar)
+        
         print(f"\nMesa Atual: {mesa}\n")
+        
+        # COMPRA DO MONTE!
         compra_monte = 0
         while len(posicoes_possiveis(mesa, jogadores_pecas[0])) == 0 and len(jogadores_pecas[0]) > 0:
             jogadores_pecas[0].append(monte[0])
             monte.pop(0)
             compra_monte += 1
-            print(chalk.red(f"\nVocê não tem peças! Comprou {compra_monte} peças do monte! \n"))
+            print(chalk.red.bold(f"\nVocê não tem peças! Comprou {compra_monte} peças do monte! \n"))
     formata()
-    #for i in range(len(ordem)):
-        #print(f'Jogador {ordem[i]}: ')
-    
+
+
 
 
