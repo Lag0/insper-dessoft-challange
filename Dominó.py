71c7f6ffaf7d33e2b84b0edf770f190a3b2cb1ff
Luca Mizrahi <lucam1@al.insper.edu.br>
2021-11-07 15:41:35 -0300

    Algumas alterações

--- Dominó.py
+++ Dominó.py
@@ -1,4 +1,5 @@
 # Importando funções e bibliotecas
+# Acertando o jogo do bruno
 
 from random import *
 from main import *
@@ -27,14 +28,27 @@ random.shuffle(ordem)
 
 print(f'No jogo temos: \n{n_jogadores} jogadores com 7 peças cada!\n')
 
+def chama_mesa():
+    print(chalk.blue(f'Tamanho do monte: {len(monte)} peças'))
+    print(chalk.yellow(f"Mesa Atual: {mesa}\n"))
+
+
 # Função de formatação do jogo
 game_on = True
 while game_on:
     for i in ordem:
+        def comprando_monte():
+            jogadores_pecas[i].append(monte[0])
+            monte.pop(0)
+
+            print(chalk.red(f"Não tem peças possíveis, comprando do monte"))
+            print(f'{jogadores_pecas[i]}\n') # Tirar esse print quando o código estiver pronto
+        
+
+ 
+
         def formata():
             
-            print(chalk.blue(f'Tamanho do monte: {len(monte)} peças'))
-            print(chalk.yellow(f"Mesa Atual: {mesa}\n"))
 
             print("-"*30)
 
@@ -49,65 +63,56 @@ while game_on:
             if len(posicoes_possiveis(mesa, jogadores_pecas[i])) >= 1  and i == 0 :
                 print("\nPosições das peças possíveis: ")
                 print(posicoes_possiveis(mesa, jogadores_pecas[i])) 
-
-
-            if i == 0:
                 jogar = int(input('Qual peça você quer jogar: '))
                 adiciona_na_mesa(jogadores_pecas[i][jogar], mesa)
                 jogadores_pecas[i].pop(jogar)
-
-            # COMPRA DO MONTE!(Manual)
-            compra_monte = 0
-            while len(posicoes_possiveis(mesa, jogadores_pecas[0])) == 0 and len(jogadores_pecas[0]) > 0: # Apenas o jogador manual deve apertar ENTER para comprar peças
-                user_input = input(
-                    chalk.red.bold(
-                        '\nVocê não tem peças possiveis! Aperte [ENTER] para comprar do monte!\n'
-                    )
-                )
-
-                if user_input == '':
-                    jogadores_pecas[0].append(monte[0])
-                    monte.pop(0)
-                    compra_monte += 1
-                else:
-                    print(chalk.red.bold("Opção Invalida! Precisione a tecla [ENTER]\n"))
-                print(chalk.red(f"Você comprou {compra_monte} peça(s) nessa rodada\n"))
+                chama_mesa()
             
             # Jogador Automático
             if i > 0:
                 a = posicoes_possiveis(mesa,jogadores_pecas[i])
                 if len(a) == 0:
-                    compra_monte2 = 0
-                    while len(posicoes_possiveis(mesa, jogadores_pecas[i])) == 0 and len(jogadores_pecas[i]) > 0: # Esse while não está funcionando - Quando o robo tira a peça que é possível de jogar o loop, porém ele não joga a peça possível na mesa
-                            jogadores_pecas[i].append(monte[0])
-                            monte.pop(0)
-                            compra_monte2 += 1
+                    while len(posicoes_possiveis(mesa, jogadores_pecas[i])) == 0 and len(jogadores_pecas[i]) > 0 and len(monte) > 0: # Esse while não está funcionando - Quando o robo tira a peça que é possível de jogar o loop, porém ele não joga a peça possível na mesa
+                        comprando_monte()
 
-                            print(chalk.red(f"O Jogador {i} comprou {compra_monte2} peça(s) nessa rodada"))
-                            print(f'{jogadores_pecas[i]}\n') # Tirar esse print quando o código estiver pronto
-                            
                 a = posicoes_possiveis(mesa,jogadores_pecas[i])
                 print(jogadores_pecas[i])
                 print(f'Posições possíveis{a}')
                 # print(f'{jogadores_pecas[i][a[0]]}\n') 
                 adiciona_na_mesa(jogadores_pecas[i][a[0]], mesa) # Por causa do else esse a só pode ser igual a 0 - PROBLEMA
                 jogadores_pecas[i].pop(a[0])
+            
+            # COMPRA DO MONTE!(Manual)
+            if i == 0 :
+                while len(posicoes_possiveis(mesa, jogadores_pecas[i])) == 0 and len(jogadores_pecas[i]) > 0 and len(monte) > 0: # Apenas o jogador manual deve apertar ENTER para comprar peças
+                    user_input = input(
+                        chalk.red.bold(
+                            '\nVocê não tem peças possiveis! Aperte [ENTER] para comprar do monte!\n'
+                        )
+                    )
+                    
+
+                    if user_input == '':
+                        comprando_monte()
+                    else:
+                        print(chalk.red.bold("Opção Invalida! Precisione a tecla [ENTER]\n"))
+                    print(chalk.red(f"Você não tem peças, comprando do monte\n"))
                     
                 
             if len(monte) == 0 and len(posicoes_possiveis(mesa, jogadores_pecas[i])) == 0:
-                for c in ordem:
-                    pontuacao = soma_pecas(jogadores_pecas[c])
+                print('Não tem peças possíveis e o monte está vazio, perde a vez')
 
-                    print(chalk.green(f"No desempate, o ganhador foi o jogador {i}!!")) # Precisa de ajuste
+                formata()
 
-                quit()
+
                 
             if len(jogadores_pecas[i]) == 0:
                 print(chalk.green(f"Jogo acabou! \nO ganhador foi o jogador {i}!!"))
                 quit()
-                
+
+        chama_mesa()      
         formata()
 
-
+#print(chalk.green(f"No desempate, o ganhador foi o jogador {i}!!")) # Precisa de ajuste
 
 
